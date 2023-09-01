2f5c3e80cf8952ef0e99ef2898240360d92ca27c
jinalshah28 <jinal28m.shah@gmail.com>
2023-09-01 17:15:17 +0530

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent,
+    RegistrationListComponent,
+   
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule, MatInputModule, MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
